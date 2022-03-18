80c2b5970537e8855aa307d656a9cfeb00d96fd3
Elizabeth Subhiksha <esubivictoria129@gmail.com>
2022-03-18 16:30:34 +0530

    download button fixed

--- src/pages/BrandById.jsx
+++ src/pages/BrandById.jsx
@@ -2,7 +2,7 @@ import Layout from "../layouts/Layout";
 import { useParams } from "react-router-dom";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import data from "../assets/files/data.json";
-import { MdDownload } from 'react-icons/md';
+import { MdDownload } from "react-icons/md";
 
 const mockupImages = [
   {
@@ -143,8 +143,8 @@ export default function BrandById() {
         </div>
       </div>
       <div className="text-center">
-        <div className="py-3 btn btn-success btn-lg h1 fw-bold text-white">
-          <p>Download Assets ⬇️</p>
+        <div className="py-2 btn btn-primary btn-lg h1 fw-bold text-white">
+          Download Assets <MdDownload />
         </div>
       </div>
     </Layout>
